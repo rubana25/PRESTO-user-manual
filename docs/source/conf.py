ce3f99159a96fb801c7177fefab2f86a23ad707d
rubana25 <rubana.aafreen@thoughtworks.com>
2021-12-24 15:50:10 +0530

    added author

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,8 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PRESTO User Manual'
+author = 'Rubana'
 
 release = '0.1'
 version = '0.1.0'
